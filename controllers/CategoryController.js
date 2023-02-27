37cedac8cf03b875c5c6fdebd8f538a480886a26
Rodolpho <redsmedeiros@gmail.com>
2023-02-27 20:35:18 -0300

    implementação das categorias

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -27,3 +27,65 @@ export const createCategoryCtrl = expressAsyncHandler(async(req, res)=>{
         category
     })
 })
+
+//@desc Get all category
+//@route GET /api/v1/categories
+//@access Public
+
+export const getAllCategoryCtrl = expressAsyncHandler(async (req, res)=>{
+
+    const categories = await Category.find()
+
+    res.json({
+        status: "success",
+        message: "Categories fetched successfully",
+        categories
+    })
+
+})
+
+//@desc Get single category
+//@route GET /api/v1/categories/:id
+//@access Public
+
+export const getSingleCategoryCtrl = expressAsyncHandler(async (req, res)=>{
+
+    const category = await Category.findById(req.params.id)
+
+    res.json({
+        status: "success",
+        message: "Category fetched successfully",
+        category
+    })
+
+})
+
+//@desc Update single category
+//@route PUT /api/v1/categories/:id
+//@access Private/Admin
+export const updateCategoryCtrl = expressAsyncHandler(async (req, res)=>{
+
+    const { name } = req.body
+
+    const category = Category.findByIdAndUpdate(req.params.id, { name }, { new: true})
+
+    res.json({
+        status: "success",
+        message: "category updated successfully",
+        category
+    })
+
+})
+
+//@desc Update single category
+//@route PUT /api/v1/categories/:id
+//@access Private/Admin
+export const deleteCategoryCtrl = expressAsyncHandler( async (req, res)=>{
+
+    await Category.findByIdAndDelete(req.params.id)
+
+    res.json({
+        status: "success",
+        message: "Product deleted successfully"
+    })
+})
--- routes/CategoryRoutes.js
+++ routes/CategoryRoutes.js
@@ -2,12 +2,16 @@ import express from "express";
 
 const categoryRoutes = express.Router()
 
-import { createCategoryCtrl } from '../controllers/CategoryController.js'
+import { createCategoryCtrl, deleteCategoryCtrl, getAllCategoryCtrl, getSingleCategoryCtrl, updateCategoryCtrl } from '../controllers/CategoryController.js'
 
 //importações de middlewares
 import { isLoggedIN } from '../middlewares/isLoggedIn.js';
 
 //rotas
-categoryRoutes.post('/', createCategoryCtrl)
+categoryRoutes.post('/', isLoggedIN, createCategoryCtrl)
+categoryRoutes.get('/', isLoggedIN, getAllCategoryCtrl)
+categoryRoutes.get('/:id', isLoggedIN, getSingleCategoryCtrl)
+categoryRoutes.put('/:id', isLoggedIN, updateCategoryCtrl)
+categoryRoutes.delete('/:id/delete', isLoggedIN, deleteCategoryCtrl)
 
 export default categoryRoutes;
